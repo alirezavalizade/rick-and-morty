2778fafc77209bbe3ef8f8f662cf8360df5ef4fc
alireza valizade <alirezavalizade0@gmail.com>
2020-07-11 22:45:10 +0200

    Added helpers

---
+++ helpers/constants.js
@@ -0,0 +1,40 @@
+export const URLS = {
+    CHARACTERS: 'character',
+    LOCATIONS: 'location',
+    EPISODES: 'episode'
+};
+
+export const FIELD_ITEMS = {
+    GENDERS: [
+        {
+            label: 'Female',
+            value: 'female',
+        },
+        {
+            label: 'Male',
+            value: 'male',
+        },
+        {
+            label: 'Genderless',
+            value: 'genderless',
+        },
+        {
+            label: 'Unknown',
+            value: 'unknown',
+        },
+    ],
+    STATUSES: [
+        {
+            label: 'Alive',
+            value: 'alive'
+        },
+        {
+            label: 'Dead',
+            value: 'dead'
+        },
+        {
+            label: 'Unknown',
+            value: 'unknown'
+        },
+    ]
+};
---
+++ helpers/index.js
@@ -0,0 +1,48 @@
+export const infiniteLoader = async (asyncFunction) => {
+    const data = [];
+    let page = 1;
+    const load = async () => {
+        try {
+            const response = await asyncFunction({
+                page
+            });
+            data.push(...response.results);
+            page++;
+            if (response.info.next) {
+                await load();
+            }
+        } catch (e) {}
+    };
+    try {
+        await load();
+    } catch (e) {
+        console.log(e);
+    } finally {
+        return data;
+    }
+};
+
+export const filterCharactersByLocationId = (characters, locationId) => {
+    return characters.filter(item => {
+        return item.location.url.includes(`/${locationId}`);
+    });
+};
+
+export const filterCharactersByEpisodeId = (characters, episodeId) => {
+    return characters.filter(item => {
+        return item.episode.some((episode) => {
+            return episode.includes(`/${episodeId}`);
+        });
+    });
+};
+
+export const getVariantColorByStatus = (status) => {
+    switch (status) {
+        case 'Dead':
+            return 'red';
+        case 'Alive':
+            return 'green';
+        default: 
+            return;
+    }
+};
