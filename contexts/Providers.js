6e1ede2425be52241bf31ca3cf4bac9412a5f62e
alireza valizade <alirezavalizade0@gmail.com>
2020-07-11 22:45:25 +0200

    Added app providers - used RecoildRoot

---
+++ contexts/Providers.js
@@ -0,0 +1,18 @@
+import { Global } from '@emotion/core';
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import { theme, globalStyles } from '@theme';
+import { RecoilRoot } from "recoil";
+
+const Providers = ({ children }) => {
+    return (
+        <ThemeProvider theme={theme}>
+            <CSSReset />
+            <Global styles={globalStyles} />
+            <RecoilRoot>
+                {children}
+            </RecoilRoot>
+        </ThemeProvider>
+    );
+};
+
+export default Providers;
