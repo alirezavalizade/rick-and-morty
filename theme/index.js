608361c6b971bb5cf34a4fc3c9d565d0e3e69836
alireza valizade <alirezavalizade0@gmail.com>
2020-07-11 22:44:58 +0200

    Added theme

---
+++ theme/index.js
@@ -0,0 +1,20 @@
+import { css } from '@emotion/core';
+import { theme as chakraTheme } from "@chakra-ui/core";
+
+export const theme = {
+    ...chakraTheme,
+    colors: {
+        ...chakraTheme.colors,
+        customGreen: '#73db35',
+        haiti: '#0C0F29',
+        fadedHaiti: '#0e1238',
+    },
+};
+
+export const globalStyles = css`
+    body {
+        background-color: ${theme.colors.haiti};
+        color: #FFF;
+        background-image: url("${require('@public/vectors/background.js').default}");
+    }
+`;
